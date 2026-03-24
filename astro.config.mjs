13ab2b4afdefcb1d367049f1a26bde3e45c786ca
Catherine Chen <bluebirid228@gmail.com>
2026-03-24 13:38:46 +0800

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import icon from "astro-icon"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,5 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [icon()],
+    integrations: [icon(), react()],
 })
