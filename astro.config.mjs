411c9507e740437c13aa9f36c3101f9a1e5bc1f8
Catherine Chen <bluebirid228@gmail.com>
2026-03-24 13:51:47 +0800

    add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import icon from "astro-icon"
 import react from "@astrojs/react"
+import node from "@astrojs/node"
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,8 @@ export default defineConfig({
         plugins: [tailwindcss()],
     },
     integrations: [icon(), react()],
+    adapter: node({
+        mode: "standalone",
+    }),
+    output: "server",
 })
